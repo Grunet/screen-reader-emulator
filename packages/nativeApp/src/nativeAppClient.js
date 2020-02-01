8822d0340e462c5dde6d189eb27a7a13508b8847
Grunet <sadikshahidain@gmail.com>
2020-02-01 12:23:55 -0600

    Creating a client for the background script to connect to the native app with

---
+++ packages/nativeApp/src/nativeAppClient.js
@@ -0,0 +1,11 @@
+import { createStreamsFromPort } from "../../extension/lib/src/portToStreams.js";
+
+function connectToNativeApp() {
+  let port = browser.runtime.connectNative(
+    "TO DO - replace this by something that can be switched out for the native app id during the webpack build"
+  );
+
+  return createStreamsFromPort(port);
+}
+
+export { connectToNativeApp };
