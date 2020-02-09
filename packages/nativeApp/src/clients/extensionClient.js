491f94c5625fa8ff2369796346ee5dda5c581d99
Grunet <sadikshahidain@gmail.com>
2020-02-08 22:48:21 -0600

    Adding a comment for an unexpected behavior of runtime.connectNative

--- packages/nativeApp/src/clients/extensionClient.js
+++ packages/nativeApp/src/clients/extensionClient.js
@@ -3,7 +3,7 @@ import { createStreamsFromPort } from "../../../lib/src/portToStreams.js";
 /* global NATIVE_APP_ID */
 
 function connectToNativeApp() {
-  const port = browser.runtime.connectNative(NATIVE_APP_ID);
+  const port = browser.runtime.connectNative(NATIVE_APP_ID); //FYI this seems to still create a port even if it can't connect to the native app
 
   return createStreamsFromPort(port);
 }
