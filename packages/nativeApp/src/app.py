0e4b23a0b97d754d9631ac536d17b6f876fabf26
Grunet <sadikshahidain@gmail.com>
2020-03-29 16:40:28 -0500

    Adding an instance of the adapter into the main program

--- packages/nativeApp/src/app.py
+++ packages/nativeApp/src/app.py
@@ -3,13 +3,20 @@ import asyncio
 
 from nativeApp.src.adapters.extensionClientAdapter import createConnectionToExtension
 
+from nativeApp.src.adapters.nvdaClientAdapter import createConnectionToNVDA
+
 
 async def main():
     extensionConnection = createConnectionToExtension(sys.stdin, sys.stdout)
 
     extensionConnection.inputStream.on_next("This is from the native app")
-    # extensionConnection.outputStream.subscribe(on_next=lambda msg: print(msg))
     extensionConnection.outputStream.subscribe(
+        on_next=lambda msg: extensionConnection.inputStream.on_next(msg)
+    )
+
+    # If/when a user can pick a screen reader to use, this should be done conditionally
+    nvdaConnection = createConnectionToNVDA()
+    nvdaConnection.outputStream.subscribe(
         on_next=lambda msg: extensionConnection.inputStream.on_next(msg)
     )
 
