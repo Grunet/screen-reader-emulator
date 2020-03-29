0f23d892f1f23e67061bafbd5a1efd66b5371a68
Grunet <sadikshahidain@gmail.com>
2020-03-29 16:09:53 -0500

    Adding in a constructor for the plugin

--- packages/plugins/nvda/src/plugin.py
+++ packages/plugins/nvda/src/plugin.py
@@ -5,6 +5,9 @@ import ui
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    def __init__(self):
+        super(globalPluginHandler.GlobalPlugin, self).__init__()
+
     @script(gesture="kb:NVDA+shift+v")
     def script_announceNVDAVersion(self, gesture):
         ui.message("Is this working?")
