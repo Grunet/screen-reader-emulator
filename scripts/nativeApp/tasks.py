1eab110e11dc5a4340d4ea8563824fa964d3c1f8
Grunet <sadikshahidain@gmail.com>
2020-05-19 15:01:27 -0500

    Factoring out the code that finds where the dependencies from the venv live into it's own file

--- scripts/nativeApp/tasks.py
+++ scripts/nativeApp/tasks.py
@@ -2,6 +2,7 @@ from invoke import task
 
 import constantsExtractor
 from dirFinder import findMatchingOutDir, findMatchingPkgDir
+from venvFinder import getPathToPkgDependencies
 
 from pathlib import Path
 import json
@@ -74,51 +75,6 @@ def __createExeFromCopiedSrc(rootDir, pkgDir, relPathToEntryPoint):
     )
 
     return __getPathToExe(rootDir, relPathToEntryPoint)
-
-
-def getPathToPkgDependencies(pkgDir):
-    absPathToPkgVirtualEnv = __getPathToPkgVirtualEnv(pkgDir)
-
-    absPathToDependencies = absPathToPkgVirtualEnv / "Lib" / "site-packages"
-
-    return absPathToDependencies
-
-
-def __getPathToPkgVirtualEnv(pkgDir):
-    alreadyTriedToInstallMissingVirtualEnv = False
-
-    while True:
-        doesVirtualEnvExist, objPathToPkgVirtualEnv = __checkForPkgVirtualEnv(pkgDir)
-
-        if doesVirtualEnvExist is True:
-            return objPathToPkgVirtualEnv
-
-        if not alreadyTriedToInstallMissingVirtualEnv:
-            subprocess.call(["pipenv", "install"], cwd=pkgDir)
-
-            alreadyTriedToInstallMissingVirtualEnv = True
-        else:
-            raise FileNotFoundError(
-                "Attempted to install the virtualenv from the Pipfile,"
-                / " but still couldn't find it"
-            )
-
-
-def __checkForPkgVirtualEnv(pkgDir):
-    strPathToPkgVirtualEnv = (
-        subprocess.run(["pipenv", "--venv"], cwd=pkgDir, stdout=subprocess.PIPE)
-        .stdout.decode("utf-8")
-        .strip()
-    )
-
-    objPathToPkgVirtualEnv = Path(strPathToPkgVirtualEnv)
-
-    # The former check is needed since the latter returns true for Path("")
-    doesPathExist = (
-        len(strPathToPkgVirtualEnv) != 0
-    ) and objPathToPkgVirtualEnv.exists()
-
-    return doesPathExist, objPathToPkgVirtualEnv
 
 
 def __getPathToExe(rootDir, relPathToEntryPoint):
