9b4930691df4163f1a3e8f94377c5a2b2a893d2b
Grunet <sadikshahidain@gmail.com>
2021-05-01 12:49:39 -0500

    Re-writing the web-ext script to use its "new" Node API

--- scripts/extension/launch-browser.js
+++ scripts/extension/launch-browser.js
@@ -1,41 +1,19 @@
-const path = require("path");
-const {
-  exec,
-  // spawn - wasn't able to get this working because of issues similar to the ones here:
-  // https://github.com/nodejs/node/issues/3675
-  // https://stackoverflow.com/questions/17516772/using-nodejss-spawn-causes-unknown-option-and-error-spawn-enoent-err/17537559#17537559
-} = require("child_process");
+const webExt = require("web-ext");
+const { findMatchingOutDir } = require("./dirFinder.js");
 
-const params = __getParametersForLaunch();
+const outDir = findMatchingOutDir(__dirname);
 
-exec(params.cmd, { cwd: params.cwd });
-//webExtProcess.on("message", msg => console.log(msg)); //Only writes anything after termintation. Need to use "spawn" to get the realtime output
+(async function () {
+  //webExt.util.logger.consoleStream.makeVerbose();
 
-function __getParametersForLaunch() {
-  const { findMatchingOutDir } = require("./dirFinder.js");
-
-  const outDir = findMatchingOutDir(__dirname);
-  const absPathToWebExtBinary = path.resolve(
-    __dirname,
-    path.normalize("./node_modules/.bin/web-ext")
+  const extensionRunner = await webExt.cmd.run(
+    {
+      sourceDir: outDir.abs,
+    },
+    {
+      shouldExitProgram: false,
+    }
   );
 
-  const relPathToWebExtBinary = path.relative(
-    outDir.abs,
-    absPathToWebExtBinary
-  ); //This can't have any spaces in it (at least on Windows)
-
-  const launchBrowserWithExtCmd = __getCmdToExecute(
-    relPathToWebExtBinary,
-    "run"
-  );
-
-  return {
-    cmd: launchBrowserWithExtCmd,
-    cwd: outDir.abs,
-  };
-}
-
-function __getCmdToExecute(pathToWebExtBinary, subCommand) {
-  return `${pathToWebExtBinary} ${subCommand}`;
-}
+  console.log(extensionRunner);
+})();
